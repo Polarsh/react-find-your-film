a90312a278a7ff1468e40686dbaf4c1bdd7bc6eb
Yorch Sanchez <dybsm98@outlook.es>
2025-11-12 18:33:27 -0500

    chore: instalar e integrar React Query para manejo de datos remotos

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import "./index.css";
 import "./i18n/index";
 
-import { RouterProvider } from "react-router-dom";
-
 import { router } from "./router";
+
+const qc = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={qc}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>
 );
